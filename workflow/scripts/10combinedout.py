d30b5782d5af8b3f8cb87e1c9f05a479ee17da5b
fschjgi <fschulz@lbl.gov>
2023-03-08 00:54:09 -0800

    minor fix

--- workflow/scripts/10combinedout.py
+++ workflow/scripts/10combinedout.py
@@ -11,9 +11,14 @@ def main(resultsdirparent, combinedout):
     """
     Retain only annotation for d_NCLDV at highest available stringency.
     """
-    df_results_f = pd.concat(
-        [pd.read_csv(f, sep="\t") for f in glob.glob(f"{resultsdirparent}/*/*.summary.tab")]
-    )
+    results = []
+    for sumout in glob.glob(resultsdirparent + "*/*.summary.tab"):
+        try:
+            df = pd.read_csv(sumout, index_col=None, header=0, sep="\t")
+            results.append(df)
+        except:
+            print (sumout + " is empty")
+    df_results_f = pd.concat(results, axis=0, ignore_index=True)
     df_results_f = df_results_f.fillna(0)
     # also output d_NCLDV-EUK, d_NCLDV-BAC, d_NCLDV-ARC, d_NCLDV-PHAGE
     df_results_f_gt3 = df_results_f[(df_results_f['domain'].str.startswith('d_NCLDV')) & (df_results_f['stringency'] == 'gte3')]
