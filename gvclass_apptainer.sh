d05f1475d3951f45c22c295e5b71d11522021b7e
Tomas Bruna <bruna.tomas@gmail.com>
2024-10-28 09:37:27 -0700

    Make apptainer calls more isolated from the host environment
    In some setups, the apptainer calls without --containall
    may clash with locally installed python packages.

--- gvclass_apptainer.sh
+++ gvclass_apptainer.sh
@@ -1,12 +1,12 @@
 QUERYDIR=$1
 PROCESSES=$2
 
-apptainer run \
+apptainer run --containall --bind $(pwd):/workdir --pwd /workdir  \
   docker://docker.io/doejgi/gvclass:latest \
   snakemake --snakefile /gvclass/workflow/Snakefile \
            -j $PROCESSES \
            --use-conda \
            --conda-frontend mamba \
            --conda-prefix /gvclass/.snakemake/conda \
-           --config querydir="$QUERYDIR" \
+           --config querydir="/workdir/$QUERYDIR" \
            database_path="/gvclass/resources"
